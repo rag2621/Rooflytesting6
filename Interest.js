a32c5940fc6ad57a0852f1327b407d2d6107a9d0
rag2621 <raghavdhima2005@gmail.com>
2025-04-13 20:16:49 +0530

    Remove .env from tracking

--- Interest.js
+++
@@ -1,5 +0,0 @@
-let inter =require("mongoose");
-
-let sch= new inter.Schema({Name:String,Email:String,Phone:String,PropertyId:String});
-
-module.exports=inter.model('Interest',sch);
--- broker.js
+++
@@ -1,5 +0,0 @@
-let inter =require("mongoose");
-
-let sch= new inter.Schema({Name:String,Email:String,Pass:String,Country:String,City:String,KYC:String});
-
-module.exports=inter.model('Broker',sch);
