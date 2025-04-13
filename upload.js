f222e2c98c2bf52590bc1667e53d081f57acf989
rag2621 <raghavdhima2005@gmail.com>
2025-04-13 20:27:12 +0530

    Removed .env from git cache and added to .gitignore

--- upload.js
+++
@@ -1,5 +0,0 @@
-let mon =require("mongoose");
-
-let sch= new mon.Schema({Title:String,Type:String,Bed:String,Bath:String, City:String,Location:String,Images:Array,Description:String,Area:Number,PropertyId:String,Amenities:Array,Email:String});
-
-module.exports=mon.model('Upload',sch);
